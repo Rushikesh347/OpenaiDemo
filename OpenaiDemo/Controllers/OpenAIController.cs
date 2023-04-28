f3ab6d028d5deeeadfb2affe12c5900f8df44c24
Teleindia <Teleindia@DESKTOP-UKQ3UC8>
2023-04-28 10:00:38 +0530

    Add project files.

---
+++ OpenaiDemo/Controllers/OpenAIController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OpenAI_API;
+using OpenAI_API.Completions;
+
+namespace OpenAiIntegration.Controllers
+{
+    public class OpenAIController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        [Route("getresult")]
+        public IActionResult GetResult([FromBody] string prompt)
+        {
+            string apikey = "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";
+            string answer = string.Empty;
+            var openai = new OpenAIAPI(apikey);
+            CompletionRequest completion = new CompletionRequest();
+            completion.Prompt = prompt;
+            completion.Model = OpenAI_API.Models.Model.DavinciText;
+            completion.MaxTokens = 200;
+            var result = openai.Completions.CreateCompletionsAsync(completion);
+            if (result != null)
+            {
+                foreach (var item in result.Result.Completions)
+                {
+                    answer = item.Text;
+                }
+                return Ok(answer);
+            }
+            else
+            {
+                return BadRequest("Not Found");
+            }
+        }
+
+    }
+}
